d4a4f93bffd13bcb86e460b50f402b042cf3ed68
Matt Gleich <git@mattglei.ch>
2022-10-13 16:13:15 -0400

    feat: load spec
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
+import { getInput } from '@actions/core';
+import { readFile } from 'fs-extra';
+
 export async function main() {
-  console.log('Hello world!');
+  const raw_spec = await loadSpec();
+}
+
+async function loadSpec(): Promise<string> {
+  const raw_spec_path = getInput('openapi_path', { required: true });
+  const raw_spec = await readFile(raw_spec_path);
+  console.log('Loaded spec from', raw_spec_path);
+  return raw_spec.toString();
 }
 
 if (require.main === module) {
