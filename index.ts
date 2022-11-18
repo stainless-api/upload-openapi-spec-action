0dd679a0551a2dadea8775496eba900bc98f743b
David Balatero <david@stainlessapi.com>
2022-11-18 14:59:10 -0500

    Refactor our Github Action to use signed uploads instead of directly uploading to the Stainless API

--- index.ts
+++ index.ts
@@ -1,44 +1,103 @@
+import fs from 'fs';
 import { getInput } from '@actions/core';
 import { error, info } from 'console';
-import { readFile, writeFile } from 'fs-extra';
+import { writeFile } from 'fs-extra';
 import fetch from 'node-fetch';
+import FormData from 'form-data';
 
 export async function main() {
   // inputs
   const stainless_api_key = getInput('stainless_api_key', { required: true });
-  const input_path = getInput('input_path', { required: true });
-  const output_path = getInput('output_path');
+  const inputPath = getInput('input_path', { required: true });
+  const outputPath = getInput('output_path');
 
-  const raw_spec = await loadSpec(input_path);
-  const decoratedSpec = await decorateSpec(raw_spec, stainless_api_key);
-  if (!output_path) {
-    return;
+  const decoratedSpec = await decorateSpec(inputPath, stainless_api_key);
+
+  if (outputPath) {
+    writeFile(outputPath, decoratedSpec);
+    info('Wrote spec to', outputPath);
   }
-  writeFile(output_path, decoratedSpec);
-  info('Wrote spec to', output_path);
 }
 
-async function loadSpec(path: string): Promise<string> {
-  const raw_spec = await readFile(path);
-  info('Loaded spec from', path);
-  return raw_spec.toString();
-}
+type SignedUploadResponse = {
+  // Which key the upload is going to
+  fileKey: string;
 
-async function decorateSpec(raw_spec: string, token: string): Promise<string> {
-  info('Decorating spec...');
-  const response = await fetch('https:/api.stainlessapi.com/api/spec', {
+  // Which URL to POST an upload to
+  url: string;
+
+  // Which fields to include in the POST
+  fields: { [key: string]: string };
+};
+
+async function createSignedUpload(token: string): Promise<SignedUploadResponse> {
+  const response = await fetch('https://api.stainlessapi.com/api/spec/upload', {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${token}`,
-      'Content-Type': 'text/plain; charset=utf-8',
+      'Content-Type': 'application/json',
     },
-    body: raw_spec,
   });
+
+  if (!response.ok) {
+    const errorMsg = `Failed to create a signed upload: ${response.statusText} ${response.text}`;
+    error(errorMsg);
+    throw Error(errorMsg);
+  }
+
+  return response.json() as unknown as SignedUploadResponse;
+}
+
+async function uploadSpec(specPath: string, upload: SignedUploadResponse) {
+  const { fields, url } = upload;
+  const formData = new FormData();
+
+  // Add the required fields for S3 upload
+  Object.entries(fields).forEach(([key, value]) => {
+    formData.append(key, value as string);
+  });
+
+  // Attach the actual spec file
+  const stats = fs.statSync(specPath);
+  formData.append('file', fs.createReadStream(specPath), { knownLength: stats.size });
+
+  const response = await fetch(url, {
+    method: 'POST',
+    body: formData,
+  });
+
+  if (!response.ok) {
+    const errorMsg = `Failed to upload spec: ${response.statusText} ${response.text}`;
+    error(errorMsg);
+    throw Error(errorMsg);
+  }
+}
+
+async function decorateSpec(specPath: string, token: string): Promise<string> {
+  info('Getting a signed upload URL...');
+  const signedUpload = await createSignedUpload(token);
+
+  info('Uploading the spec file...');
+  await uploadSpec(specPath, signedUpload);
+
+  info('Decorating spec...');
+  const response = await fetch('https://api.stainlessapi.com/api/spec', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      uploadedFileKey: signedUpload.fileKey,
+    }),
+  });
+
   if (!response.ok) {
     const errorMsg = `Failed to decorate spec: ${response.statusText} ${response.text}`;
     error(errorMsg);
     throw Error(errorMsg);
   }
+
   info('Decorated spec');
   return response.text();
 }
