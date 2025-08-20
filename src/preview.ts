32d2a15b65208bcd4f720be949dd994171fb704c
meorphis <108296353+meorphis@users.noreply.github.com>
2025-08-20 12:54:21 -0400

    use base content for base build, even if it differs from base revision (#104)
    * use base content for base build, even if it differs from base revision
    
    * create branch up front
    
    * comment
    
    * fix
    
    ---------
    
    Co-authored-by: meorphis <eric@stainless.com>

--- src/preview.ts
+++ src/preview.ts
@@ -107,7 +107,7 @@ async function main() {
       return;
     }
 
-    const baseRevision = await computeBaseRevision({
+    const branchFrom = await computeBranchFrom({
       stainless,
       projectName,
       mergeBaseConfig,
@@ -134,8 +134,10 @@ async function main() {
       stainless,
       oasContent: headConfig.oas,
       configContent: headConfig.config,
+      baseOasContent: mergeBaseConfig.oas,
+      baseConfigContent: mergeBaseConfig.config,
       projectName,
-      baseRevision,
+      branchFrom,
       baseBranch,
       branch,
       guessConfig: !configPath && !!oasPath,
@@ -196,7 +198,7 @@ async function main() {
   }
 }
 
-async function computeBaseRevision({
+async function computeBranchFrom({
   stainless,
   projectName,
   mergeBaseConfig,
