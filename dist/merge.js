102dc971cb22d692f134a4bc76319bb72b1ff7a5
CJ Quines <cjquines0@gmail.com>
2025-09-25 13:43:24 -0400

    fix: filter diagnostics by target (#125)
    * fix: filter diagnostics by target
    
    * build

--- dist/merge.js
+++ dist/merge.js
@@ -40537,7 +40537,8 @@ async function* pollBuild({
         outcomes[language].diagnostics = [];
         try {
           for await (const diagnostic of stainless.builds.diagnostics.list(
-            buildId
+            buildId,
+            { targets: [language] }
           )) {
             outcomes[language].diagnostics.push(diagnostic);
           }
