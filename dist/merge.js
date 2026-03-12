d6079596fa6826cb0f87e9fe8daad076d3818e10
meorphis <eric@stainless.com>
2026-03-12 11:38:42 -0400

    retry on 401

--- dist/merge.js
+++ dist/merge.js
@@ -19949,6 +19949,21 @@ function createAutoRefreshFetch(initialAuth, refreshAuth) {
     return fetch(input, { ...init, headers });
   };
 }
+var addFetch401Retries = (fetch2, { numRetries }) => {
+  return async (input, init) => {
+    let attempts = 0;
+    let response;
+    do {
+      response = await fetch2(input, init);
+      if (response.status !== 401) {
+        break;
+      }
+      attempts++;
+      logger.warn("Received 401 response, retrying...");
+    } while (attempts <= numRetries);
+    return response;
+  };
+};
 function getStainlessClient(action, opts) {
   const headers = {
     "User-Agent": `Stainless/Action ${package_default.version}`
@@ -19983,7 +19998,10 @@ function wrapAction(actionType, fn) {
         apiKey: auth.key,
         logLevel: "warn",
         logger,
-        fetch: createAutoRefreshFetch(auth, getStainlessAuth)
+        fetch: addFetch401Retries(
+          createAutoRefreshFetch(auth, getStainlessAuth),
+          { numRetries: 2 }
+        )
       });
       await fn(stainless);
       await maybeReportResult({
